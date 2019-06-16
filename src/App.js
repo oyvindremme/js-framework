9876f04d27384656138496a79cb96f022bee6952
LAPTOP-DVJFUS5D\oyvin <oyvind.remme@gmail.com>
2019-06-16 14:55:36 +0200

    added navigation menu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Home from './pages/Home/Home';
+import './scss/style.scss';
+import { HashRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <h1>Hey</h1>
+    <HashRouter>
+      <Navigation />
+      <Route exact path="/" component={Home} />
+    </HashRouter>
   );
 }
 
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './Navigation.scss';
+import { NavLink, Link } from 'react-router-dom';
+
+class Navigation extends Component {
+    render() { 
+        return (
+                <nav className="navigation navbar navbar-expand navbar-light bg-light">
+                    <Link to="/" className="navbar-brand">Pokemon Card Game</Link>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <NavLink to="/" className="nav-link">Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/" className="nav-link">Link</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/" className="nav-link">Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/" className="nav-link">Link</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+        );
+    }
+}
+ 
+export default Navigation;
